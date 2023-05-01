89f74bd4eb5fa6397d5c4a32b052f9b5f938a506
traviscult <60018617+traviscult@users.noreply.github.com>
2023-05-01 18:59:29 -0500

    created route and nav bar is now displaying

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import SearchPage from "./pages/search";
 import PetDetailsPage from "./pages/detail";
 import PetDetailsNotFound from "./pages/petDetailsNotFound";
 import Root from "./components/root";
+
+// Add react-router-dom imports
 import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Router,
 } from "react-router-dom";
 
-// Add react-router-dom imports
-
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+);
 
 function App() {
   return <RouterProvider router={appRouter} />;
