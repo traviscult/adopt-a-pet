e07670b295bb1b07b5e4f73a9bda3f13359617ad
traviscult <60018617+traviscult@users.noreply.github.com>
2023-05-10 16:52:35 -0500

    imported use params allowing for filtering pets in application

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
     </Route>
   )
 );
