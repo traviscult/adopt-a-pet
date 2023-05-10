1d6f27b47a356412b7d7c7a13781aa910d84aeb9
traviscult <60018617+traviscult@users.noreply.github.com>
2023-05-10 17:05:11 -0500

    replaced anchor tags with link to prevent refresh

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getPets } from "../../api/petfinder";
 import Hero from "../../components/hero";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 // import Link
 
@@ -33,9 +33,9 @@ const HomePage = () => {
       {data.length ? (
         <div className="grid">
           {data.map((animal) => (
-            <a // Change me to a Link!
+            <Link // Change me to a Link!
               key={animal.id}
-              href={`/${animal.type.toLowerCase()}/${animal.id}`}
+              to={`/${animal.type.toLowerCase()}/${animal.id}`}
               className="pet"
             >
               <article>
@@ -53,7 +53,7 @@ const HomePage = () => {
                 <p>Color: {animal.colors.primary}</p>
                 <p>Gender: {animal.gender}</p>
               </article>
-            </a> // Don't forget to change me!
+            </Link> // Don't forget to change me!
           ))}
         </div>
       ) : (
